85a830ac5e52198f536765b33fa5f1d1cdc23a07
Jatin Yadav <jatiny.yadav8@gmail.com>
2025-12-15 11:41:21 +0530

    Search feature added and navbar updated

--- controller/listing.js
+++ controller/listing.js
@@ -1,90 +1,88 @@
 const Listing = require("../models/listing");
 
-
-// index route 
+// ✅ INDEX + SEARCH ROUTE
 module.exports.index = async (req, res) => {
-    const allListing = await Listing.find({});
+    let { q } = req.query;   // navbar se aane wali value
+    let allListing;
+
+    if (q && q.trim() !== "") {
+        allListing = await Listing.find({
+            title: { $regex: q, $options: "i" }   // case-insensitive search
+        });
+    } else {
+        allListing = await Listing.find({});
+    }
+
     res.render("listings/index", { allListing });
 };
 
-//New route
+// New route
 module.exports.renderNewForm = (req, res) => {
     res.render("listings/new.ejs");
 };
 
-
-// Show route 
-module.exports.showListing = (async (req, res) => {
+// Show route
+module.exports.showListing = async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id)
         .populate({
             path: "reviews",
-            populate: { 
-                path: "author"
-            }
+            populate: { path: "author" }
         })
         .populate("owner");
+
     if (!listing) {
-        req.flash("error", "Listing you requested dose not exist!");
-        res.redirect("/listings");
+        req.flash("error", "Listing you requested does not exist!");
+        return res.redirect("/listings");
     }
-    console.log(listing);
 
     res.render("listings/show.ejs", { listing });
-});
+};
 
-// Create route 
-module.exports.createListing = async (req, res, next) => {
-   let url = req.file.path;
-   let filename = req.file.filename;
-  
+// Create route
+module.exports.createListing = async (req, res) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    newListing.image = {url, filename};
+    newListing.image = { url, filename };
+
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
-
 };
 
-
-//Edit route 
+// Edit form
 module.exports.renderEditForm = async (req, res) => {
-    
     let { id } = req.params;
     const listing = await Listing.findById(id);
 
-    let originalImageUrl = listing.image.url;
-    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+    let originalImageUrl = listing.image.url.replace("/upload", "/upload/w_250");
     res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
-
 // Update route
-
 module.exports.updateListing = async (req, res) => {
- 
-  
     let { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-if(typeof req.file !== "undefined"){
-    let url = req.file.path;
-   let filename = req.file.filename;
-       listing.image = {url, filename};
-       await listing.save();
-}
+    if (req.file) {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
 
     req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 };
 
-
-//Delete route
+// Delete route
 module.exports.deleteListing = async (req, res) => {
     let { id } = req.params;
-    let deletedList = await Listing.findByIdAndDelete(id);
-    console.log(deletedList);
+    await Listing.findByIdAndDelete(id);
+
     req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 };
